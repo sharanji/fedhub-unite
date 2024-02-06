8cdfae60392861be60384fa0a0296d5ef450e448
sharanji <sharanjipoun@gmail.com>
2024-02-06 23:33:46 +0530

    edited prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -14,4 +14,6 @@ export const prisma =
         log: ['query']
     })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') { globalForPrisma.prisma = prisma; } else {
+    globalForPrisma.prisma = new PrismaClient();
+}
