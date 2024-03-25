aaf43d661b3f536d9c967e35a4d5c7918bf36d61
sharanji <sharanjipoun@gmail.com>
2024-03-25 23:31:02 +0530

    privacy policy issue and url

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const PrivacyPolicy = () => {
@@ -7,7 +8,7 @@ const PrivacyPolicy = () => {
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link
+      <Link
         rel="stylesheet"
         type="text/css"
         href="/public/livelink/css/livelink.css"
@@ -86,7 +87,7 @@ const PrivacyPolicy = () => {
                   <p>
                     <strong>Affiliate</strong> means an entity that controls, is
                     controlled by or is under common control with a party, where
-                    "control" means ownership of 50% or more of the shares,
+                    {"control"} means ownership of 50% or more of the shares,
                     equity interest or other securities entitled to vote for
                     election of directors or other managing authority.
                   </p>
@@ -99,9 +100,10 @@ const PrivacyPolicy = () => {
                 </li>
                 <li>
                   <p>
-                    <strong>Company</strong> (referred to as either "the
-                    Company", "We", "Us" or "Our" in this Agreement) refers to
-                    Fedhub, 40 , chaprathi village.
+                    <strong>Company</strong> (referred to as either
+                    {` "the
+                    Company", "We", "Us" or "Our"`}
+                    in this Agreement) refers to Fedhub, 40 , chaprathi village.
                   </p>
                 </li>
                 <li>
@@ -211,7 +213,7 @@ const PrivacyPolicy = () => {
                 Usage Data is collected automatically when using the Service.
               </p>
               <p>
-                Usage Data may include information such as Your Device's
+                Usage Data may include information such as Your {`Device's`}
                 Internet Protocol address (e.g. IP address), browser type,
                 browser version, the pages of our Service that You visit, the
                 time and date of Your visit, the time spent on those pages,
@@ -247,16 +249,16 @@ const PrivacyPolicy = () => {
                 If You decide to register through or otherwise grant us access
                 to a Third-Party Social Media Service, We may collect Personal
                 data that is already associated with Your Third-Party Social
-                Media Service's account, such as Your name, Your email address,
-                Your activities or Your contact list associated with that
-                account.
+                Media Service{``}s account, such as Your name, Your email
+                address, Your activities or Your contact list associated with
+                that account.
               </p>
               <p>
                 You may also have the option of sharing additional information
-                with the Company through Your Third-Party Social Media Service's
-                account. If You choose to provide such information and Personal
-                Data, during registration or otherwise, You are giving the
-                Company permission to use, share, and store it in a manner
+                with the Company through Your Third-Party Social Media Service
+                {``}s account. If You choose to provide such information and
+                Personal Data, during registration or otherwise, You are giving
+                the Company permission to use, share, and store it in a manner
                 consistent with this Privacy Policy.
               </p>
               <h4>Information Collected while Using the Application</h4>
@@ -266,15 +268,15 @@ const PrivacyPolicy = () => {
               </p>
               <ul>
                 <li>
-                  Pictures and other information from your Device's camera and
-                  photo library
+                  Pictures and other information from your Device{``}s camera
+                  and photo library
                 </li>
               </ul>
               <p>
                 We use this information to provide features of Our Service, to
                 improve and customize Our Service. The information may be
-                uploaded to the Company's servers and/or a Service Provider's
-                server or it may be simply stored on Your device.
+                uploaded to the Company{``}s servers and/or a Service Provider
+                {``}s server or it may be simply stored on Your device.
               </p>
               <p>
                 You can enable or disable access to this information at any
@@ -310,16 +312,17 @@ const PrivacyPolicy = () => {
                 </li>
               </ul>
               <p>
-                Cookies can be "Persistent" or "Session" Cookies. Persistent
-                Cookies remain on Your personal computer or mobile device when
-                You go offline, while Session Cookies are deleted as soon as You
-                close Your web browser. You can learn more about cookies on{" "}
+                Cookies can be {`"Persistent"`} or {`"Session"`} Cookies.
+                Persistent Cookies remain on Your personal computer or mobile
+                device when You go offline, while Session Cookies are deleted as
+                soon as You close Your web browser. You can learn more about
+                cookies on
                 <a
                   href="https://www.termsfeed.com/blog/cookies/#What_Are_Cookies"
                   target="_blank"
                 >
                   TermsFeed website
-                </a>{" "}
+                </a>
                 article.
               </p>
               <p>
@@ -407,7 +410,7 @@ const PrivacyPolicy = () => {
                   <p>
                     <strong>To contact You:</strong> To contact You by email,
                     telephone calls, SMS, or other equivalent forms of
-                    electronic communication, such as a mobile application's
+                    electronic communication, such as a mobile application{``}s
                     push notifications regarding updates or informative
                     communications related to the functionalities, products or
                     contracted services, including the security updates, when
@@ -517,7 +520,7 @@ const PrivacyPolicy = () => {
               <h3>Transfer of Your Personal Data</h3>
               <p>
                 Your information, including Personal Data, is processed at the
-                Company's operating offices and in any other places where the
+                Company{``}s operating offices and in any other places where the
                 parties involved in the processing are located. It means that
                 this information may be transferred to — and maintained on —
                 computers located outside of Your state, province, country or
@@ -601,7 +604,7 @@ const PrivacyPolicy = () => {
                 use commercially acceptable means to protect Your Personal Data,
                 We cannot guarantee its absolute security.
               </p>
-              <h2>Children's Privacy</h2>
+              <h2>Children{``}s Privacy</h2>
               <p>
                 Our Service does not address anyone under the age of 13. We do
                 not knowingly collect personally identifiable information from
@@ -615,15 +618,15 @@ const PrivacyPolicy = () => {
               <p>
                 If We need to rely on consent as a legal basis for processing
                 Your information and Your country requires consent from a
-                parent, We may require Your parent's consent before We collect
-                and use that information.
+                parent, We may require Your parent{``}s consent before We
+                collect and use that information.
               </p>
               <h2>Links to Other Websites</h2>
               <p>
                 Our Service may contain links to other websites that are not
                 operated by Us. If You click on a third party link, You will be
-                directed to that third party's site. We strongly advise You to
-                review the Privacy Policy of every site You visit.
+                directed to that third party{``}s site. We strongly advise You
+                to review the Privacy Policy of every site You visit.
               </p>
               <p>
                 We have no control over and assume no responsibility for the
@@ -639,7 +642,7 @@ const PrivacyPolicy = () => {
               <p>
                 We will let You know via email and/or a prominent notice on Our
                 Service, prior to the change becoming effective and update the
-                "Last updated" date at the top of this Privacy Policy.
+                {`"Last updated"`} date at the top of this Privacy Policy.
               </p>
               <p>
                 You are advised to review this Privacy Policy periodically for
